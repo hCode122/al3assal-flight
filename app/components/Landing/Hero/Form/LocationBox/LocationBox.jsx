fdb2071cc29471198d700248868b1e5090dca8b7
hCode122 <hCode122@proton.me>
2025-02-20 17:20:41 +0300

    added functionality for flip button

--- app/components/Landing/Hero/Form/LocationBox/LocationBox.jsx
+++ app/components/Landing/Hero/Form/LocationBox/LocationBox.jsx
@@ -33,6 +33,11 @@ const LocationBox = ({setFormData, formData}) => {
     ]}
     )
     
+    function flipValues() {
+        let temp = selectedT
+        setSelectedT(selectedO)
+        setSelectedO(temp)
+    }
 
     return (
         <div className="relative flex flex-col items-center w-full lg:flex-1 lg:flex-row gap-2 lg:gap-2 ">
@@ -42,7 +47,8 @@ const LocationBox = ({setFormData, formData}) => {
                 <p className="text-[#BFBFBF] text-[1.4rem] font-[400]">{selectedO ? selectedO : 'Origin'}</p>
                 {isOriginOpen ? <LocationMenu setSelectedLocation={setSelectedO} locationData={locationData['origin']} setIsOpen={setIsOriginOpen} /> : null}
             </div>
-                <Image alt="switch button" className="cursor-pointer hidden lg:block absolute left-[48%] translate-x-[-50%] top-[50%] translate-y-[-50%] z-[55]" src={switchImg}
+                <Image onClick={() => flipValues()}
+                alt="switch button" className="cursor-pointer hidden lg:block absolute left-[48%] translate-x-[-50%] top-[50%] translate-y-[-50%] z-[55]" src={switchImg}
                  height={50} width={50} />
             <div onClick={() => setIsDestinationOpen(true)} className="relative w-full lg:w-auto flex flex-col bg-[#F7F7FA] flex-1 pl-4 lg:pl-12 py-2 rounded-[8px] cursor-pointer">
                 <p className="text-[#7A7A7A] text-[1rem] font-[]">To</p>
